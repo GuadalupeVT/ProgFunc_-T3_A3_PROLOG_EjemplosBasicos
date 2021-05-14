e06c61b2e588c3401a3a143b1b7182793115a8b0
Guadalupe Vazquez <guvadlt@gmail.com>
2021-05-14 11:39:34 -0500

    Se agregaron CONSULTAS para probar cada una una de las reglas anteriores

--- BC_Personas.pl
+++ BC_Personas.pl
@@ -68,17 +68,43 @@ esposo(Z, Y) :- padre(Z, X) , madre(Y, X).
 
 %Agregar una regla para identificar a los ABUELOS.
 abuelo(Z) :- padre(Z, X) , (padre(X, Y) ; madre(X, Y)), hombre(Z).
+
 %Agregar una regla para identificar a las ABUELAS. 
 abuela(Z) :- madre(Z, X) , (padre(X, Y) ; madre(X, Y)), mujer(Z).
+
 %Agregar una regla para identificar a una PERSONA. 
 persona(P) :- hombre(P) ; mujer(P).
+
 %Agregar una regla para identificar a un NIETO
 nieto(Y) :- padre(Z, X) , (padre(X, Y) ; madre(X, Y)), hombre(Y).
+
 %Agregar una regla para identificar a un NIETA
 nieta(Y) :- madre(Z, X) , (padre(X, Y) ; madre(X, Y)), mujer(Y).
+
 %Agregar una regla para identificar a los HERMANOS
 hermanos(X,Y) :- ((madre(Z,X) , madre(Z,Y)); (padre(Z,X) , padre(Z,Y))), not(X==Y) , hombre(X), hombre(Y).
+
 %Agregar una regla para identificar a las HERMANAS
 hermanas(X,Y) :- ((madre(Z,X) , madre(Z,Y)); (padre(Z,X) , padre(Z,Y))), not(X==Y) , mujer(X), mujer(Y).
+
 %Agregar una regla para identificar a las PAREJAS.
 parejas(Z, Y) :- padre(Z, X) , madre(Y, X).
+
+
+%Agregar una CONSULTA para probar cada una una de las reglas anteriores. (Agregarlo como comentario para poder verificarlas después).
+%abuelo(ABUELO).
+%abuelo(sergio).
+%abuela(ABUELA).
+%abuela(ana).
+%persona(PERSONA).
+%persona(ana).
+%nieto(NIETO).
+%nieto(paco).
+%nieta(NIETA).
+%hermanos(H1,H2).
+%hermanos(luis,sergio).
+%hermanas(H1,H2).
+%hermanas(maria,ana).
+%parejas(ESPOSO,ESPOSA).
+
+
